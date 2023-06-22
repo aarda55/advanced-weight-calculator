68d45fde0daa3a3990c044bd127cd48dc4720081
bb129 <79632956+aarda55@users.noreply.github.com>
2023-06-22 22:18:33 +0200

    Add files via upload

---
+++ weight-calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int weight;
+int *p;
+*p = weight; 
+int weightcalc();
+
+int main(){
+    printf("Enter your weight\n");
+    scanf("%d", &weight);
+    weightcalc(weight)
+
+}
+
+int weightcalc(){
+    printf("")
+
+}
